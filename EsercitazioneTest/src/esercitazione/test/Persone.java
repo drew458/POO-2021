74b2b6aebe12393ca7014c41f368d8f59f6fb426
Andrea <andry.marini@gmail.com>
2021-03-29 17:37:05 +0200

    Lezione 29-03 - testing

---
+++ EsercitazioneTest/src/esercitazione/test/Persone.java
@@ -0,0 +1,19 @@
+package esercitazione.test;
+
+public class Persone {
+	private String[] nomi;
+	public Persone(int n) {
+		this.nomi = new String[n];
+	}
+	public int contaOmonimiDi(String nome) {
+		// metodo da scrivere
+		int omonimi=0;
+		for(String persona:this.nomi)
+			if(persona.equals(nome))
+				omonimi++;
+		return omonimi;
+	}
+	public void aggiungiNome(int indice, String nome){
+		this.nomi[indice] = nome;
+	}
+}
---
+++ EsercitazioneTest/src/test/PersoneTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import esercitazione.test.Persone;
+
+public class PersoneTest {
+
+	@Test
+	public void testOmonimiPersoneVuote() {
+		assertEquals(0, persone().contaOmonimiDi("Alice"));
+	}
+	
+	@Test
+	public void testUnOmonimo() {
+		assertEquals(1, persone("Alice").contaOmonimiDi("Alice"));
+	}
+	
+	@Test
+	public void testZeroOmonimi() {
+		assertEquals(0, persone("Bob", "Carlo").contaOmonimiDi("Alice"));
+	}
+
+
+	private Persone persone(String... nomi) {
+		Persone p = new Persone(nomi.length);
+		for(int i=0; i<nomi.length; i++)
+			p.aggiungiNome(i, nomi[i]);
+		return p;
+	}
+	
+}
