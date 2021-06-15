b7533e5dfd6739ada48ba099d78ede4b7fb98a48
drew <andry.marini@gmail.com>
2021-06-15 10:28:10 +0200

    es8

--- ProvaItinere3/src/it/completare/Prodotto.java
+++ ProvaItinere3/src/it/completare/Prodotto.java
@@ -1,14 +1,16 @@
 package it.completare;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Scrivere il metodo getProdotto2quantita() della classe OperazioniProdotti. Tale metodo riceve in input due mappe. La prima mappa,
- * cod2prod, associa un codice di tipo String ad un oggetto di tipo Prodotto. La seconda, cod2qta, associa un codice di tipo String ad una
- * quantità di tipo Integer.
+ * cod2prod, associa un codice di tipo String ad un oggetto di tipo Prodotto. La seconda, cod2qta, associa 
+ * un codice di tipo String ad una quantità di tipo Integer.
  * Il metodo deve restituire una mappa che associa i prodotti della prima mappa alle quantità della seconda mappa usando la
  * corrispondeza fra codici (le chiavi delle mappe).
- * E' possibile che per alcuni prodotti non siano riportate le quantità. In quel caso il prodotto deve essere riportato con quantità pari a -1.
+ * E' possibile che per alcuni prodotti non siano riportate le quantità. In quel caso il prodotto 
+ * deve essere riportato con quantità pari a -1.
  * N.B. Non modificare la classe Prodotto.
  *
  */
@@ -38,8 +40,15 @@ public class Prodotto {
 	//INIZIA A MODIFICARE DA QUI...
 	
 	public class OperazioniProdotti{
-		public Map<Prodotto, Integer> getProdotto2quantita(Map<String, Prodotto> cod2prodotto){
-			return null; //DA COMPLETARE
+		public Map<Prodotto, Integer> getProdotto2quantita(Map<String, Prodotto> cod2prod, Map<String, Integer> cod2qta){
+			Map<Prodotto, Integer> prod2qta = new HashMap<>();
+			
+			for(String codice : cod2prod.keySet()) {
+				if(cod2qta.containsKey(codice))
+					prod2qta.put(cod2prod.get(codice), cod2qta.get(codice));
+				else prod2qta.put(cod2prod.get(codice), -1);
+			}
+			return prod2qta;
 		}
 	}
 }
