5d2854638d42453ea95db7b41b650b92bee26a61
drew <andry.marini@gmail.com>
2021-08-30 17:30:16 +0200

    name refactor

--- estensione-protected-13/src/CalcolatoreDiAreaNonCompila.java
+++ estensione-protected-13/src/CalcolatoreDiAreaNonCompila.java
@@ -1,10 +1,8 @@
 /**
- * Compila perche si è avuto cura di far coincidere tipo statico e tipo dinamico delle variabili
- *  locali cerchio e quadrato
+ * Questo metodo invece, con Forma al posto dei tipi Cerchio e Quadrato, invece non compila
  *
  */
-public class CalcolatoreDiArea {
-	
+public class CalcolatoreDiAreaNonCompila {
 	public float areaDi(Cerchio c) {
 		int r = c.getRaggio();
 		return 3.14f *r *r;
@@ -17,10 +15,9 @@ public class CalcolatoreDiArea {
 	
 	public static void main(String args) {
 		CalcolatoreDiArea calcolatore = new CalcolatoreDiArea();
-		Cerchio cerchio = new Cerchio(1);
-		Quadrato quadrato = new Quadrato(2);
+		Forma cerchio = new Cerchio(1);
+		Forma quadrato = new Quadrato(2);
 		System.out.println(calcolatore.areaDi(cerchio));
 		System.out.println(calcolatore.areaDi(quadrato));
 	}
-
 }
